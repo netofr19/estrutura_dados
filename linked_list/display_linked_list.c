4cc10c770aab070f976a07d114742d02564a9f89
Francisco Procopio <francisconeto.cg@gmail.com>
2024-03-25 16:40:19 -0300

    reverse linked list

--- linked_list/display_linked_list.c
+++ linked_list/display_linked_list.c
@@ -166,6 +166,131 @@ void Insert(Node *p, int index, int x){
     }
 }
 
+void insertSortedList(Node *p, int x){
+    Node *t, *q=NULL;
+    t = (Node *)malloc(sizeof(Node));
+    t->data=x;
+    t->next=NULL;
+
+    if (first==NULL){
+        first=t;
+    }
+    else {
+        while(p && p->data < x){
+            q=p;
+            p = p-> next;
+        }
+        if (p==first){
+            t->next = first;
+            first = t;
+        }
+        else {
+            t->next = q->next;
+            q->next = t;
+        }
+    }
+}
+
+int Delete(Node *p, int index){
+    Node *q=NULL;
+    int x=-1;
+
+    if(index<1 || index > count(p)){
+        return -1;
+    }
+    if(index==1){
+        q=first;
+        x=first->data;
+        first = first->next;
+        free(q);
+        return x;
+    }
+    else {
+        for(int i=0; i<index-1; i++){
+            q=p;
+            p=p->next;
+        }
+        q->next=p->next;
+        x=p->data;
+        free(p);
+        return x;
+    }
+
+}
+
+int isSorted(Node *p){
+    int x = INT_MIN;
+
+    while (p!=NULL){
+        if (p->data < x){
+            return 0;
+        }
+        x=p->data;
+        p=p->next;
+    }
+    return 1;
+}
+
+void RemoveDuplicate(Node *p){
+    Node *q = p->next;
+
+    while(q!=NULL){
+        if (p->data!=q->data){
+            p=q;
+            q=q->next;
+        }
+        else{
+            p->next = q->next;
+            free(q);
+            q=p->next;
+        }
+    }
+}
+
+void Reverse1(Node *p){
+    int *A, i=0;
+    Node *q=p;
+
+    A = (int *)malloc(sizeof(int)*count(p));
+
+    while (q != NULL){
+        A[i] = q->data;
+        q = q->next;
+        i++;
+    }
+    q = p;
+    i--;
+    while (q!=NULL){
+        q->data = A[i];
+        q = q->next;
+        i--;
+    }
+
+}
+
+void Reverse2 (Node *p){
+    Node *q = NULL, *r = NULL;
+
+    while (p!=NULL){
+        r = q;
+        q = p;
+        p = p->next;
+        q->next=r;
+
+    }
+    first=q;
+}
+
+void Reverse3(Node *q, Node *p){
+    if(p){
+        Reverse3(p, p->next);
+        p->next=q;
+    }
+    else{
+        first=q;
+    }
+}
+
 int main()
 {
     int A[]={3,5,7,10,15, 8, 12, 2};
@@ -258,6 +383,70 @@ int main()
 
     printf("\n---------------------------------\n");
 
-    
+    int B[] = {10, 20, 120, 40, 5};
+    Create(B, 5);
+
+    printf("Display the Linked List B: ");
+    Display(first);
+
+    printf("\n---------------------------------\n");
+
+    printf("Display the Linked List B after insert element: ");
+    insertSortedList(first, 35);
+    Display(first);
+
+    printf("\n---------------------------------\n");
+
+    printf("Deleted value: %d\n", Delete(first, 3));
+
+    printf("Display the Linked List B after delete element: ");
+    Display(first);
+
+    printf("\n---------------------------------\n");
+
+    if(isSorted(first)){
+        printf("The linked list B is sorted!");
+    } else {
+        printf("The linked list B isn't sorted!");
+    }
+
+    printf("\n---------------------------------\n");
+
+    int C[] = {10, 10, 10, 20, 20, 20, 30, 40, 40, 40, 40, 50};
+    Create(C, 12);
+
+    printf("Linked List C before remove duplicates: ");
+    Display(first);
+
+    RemoveDuplicate(first);
+
+    printf("\nLinked List C after remove duplicates: ");
+    Display(first);
+
+    printf("\n---------------------------------\n");
+
+    int D[] = {10,20,30,40,50};
+    Create(D,5);
+
+    printf("Linked List D before reverse: ");
+    Display(first);
+
+    Reverse1(first);
+
+    printf("\nLinked List D after reverse1: ");
+    Display(first);
+
+    Reverse2(first);
+
+    printf("\nLinked List D after reverse2: ");
+    Display(first);
+
+    Reverse3(NULL, first);
+
+    printf("\nLinked List D after reverse3: ");
+    Display(first);
+
+    printf("\n---------------------------------\n");
+
     return 0;
 }
