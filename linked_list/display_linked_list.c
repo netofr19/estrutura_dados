34ec455018f8376cd59c9ee586d5b613e627f7b4
Francisco Procópio <francisco.procopio@enacom.com.br>
2024-03-26 11:41:57 -0300

    check if the linked list is a loop

--- linked_list/display_linked_list.c
+++ linked_list/display_linked_list.c
@@ -5,7 +5,7 @@
 struct Node{
     int data;
     struct Node *next;
-} *first=NULL;
+} *first=NULL, *second, *third;
 
 typedef struct Node Node;
 
@@ -16,6 +16,23 @@ void Create(int A[], int n){
     first->data=A[0];
     first->next=NULL;
     last=first;
+
+    for(i=1; i<n;i++){
+        temp=(Node *)malloc(sizeof(Node));
+        temp->data=A[i];
+        temp->next=NULL;
+        last->next=temp;
+        last=temp;
+    }
+}
+
+void Create2(int A[], int n){
+    int i;
+    Node *temp, *last;
+    second=(Node *)malloc(sizeof(Node));
+    second->data=A[0];
+    second->next=NULL;
+    last=second;
 
     for(i=1; i<n;i++){
         temp=(Node *)malloc(sizeof(Node));
@@ -291,6 +308,67 @@ void Reverse3(Node *q, Node *p){
     }
 }
 
+void Concat(Node *p, Node *q){
+    third=p;
+
+    while (p->next != NULL){
+        p=p->next;
+    }
+    p->next=q;
+}
+
+void Merge(Node *p, Node *q){
+    Node *last;
+    if (p->data < q->data){
+        third = last = p;
+        p=p->next;
+        third->next = NULL;
+    }
+    else {
+        third = last = q;
+        q = q->next;
+        third->next = NULL;
+    }
+
+    while (p && q) {
+        if (p->data < q->data){
+            last->next = p;
+            last=p;
+            p = p->next;
+            last->next = NULL;
+        }
+        else {
+            last->next = q;
+            last=q;
+            q = q->next;
+            last->next = NULL;
+        }
+    }
+    if(p){
+        last->next = p;
+    }
+    if (q){
+        last->next = q;
+    }
+}
+
+int isLoop(Node *f){
+    Node *p, *q;
+    p=q=f;
+    
+    do{
+        p=p->next;
+        q=q->next;
+        q=q?q->next:q;
+    }while(p && q && p!=q);
+    if(p==q){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
 int main()
 {
     int A[]={3,5,7,10,15, 8, 12, 2};
@@ -447,6 +525,61 @@ int main()
     Display(first);
 
     printf("\n---------------------------------\n");
+
+    int E[] = {50, 40, 10, 30, 20};
+    int F[] = {1,2,3,4,5};
+    Create(E, 5);
+    Create2(F, 5);
+
+    printf("First: ");
+    Display(first);
+    printf("\n");
+
+    printf("Second: ");
+    Display(second);
+    printf("\n");
+
+    Concat(first, second);
+
+    printf("Concatinated: ");
+    Display(third);
+
+    printf("\n---------------------------------\n");
+
+    int G[] = {10, 20, 30, 40, 50};
+    int H[] = {5,15, 25, 35, 45};
+    Create(G, 5);
+    Create2(H, 5);
+
+    printf("First: ");
+    Display(first);
+    printf("\n");
+
+    printf("Second: ");
+    Display(second);
+    printf("\n");
+
+    Merge(first, second);
+
+    printf("Merged Linked List: ");
+    Display(third);
+
+    printf("\n---------------------------------\n");
+
+    Node *t1, *t2;
+
+    int I[] = {10,20,30,40,50};
+    Create(I, 5);
+
+    t1 = first->next->next;
+    t2 = first->next->next->next->next;
+    t2->next=t1;
+
+    printf("%d", isLoop(first));
+
+    printf("\n---------------------------------\n");
+
+
 
     return 0;
 }
